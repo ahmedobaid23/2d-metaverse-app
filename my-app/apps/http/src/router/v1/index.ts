8a73d4afe961091773b618adf44c8708730df101
ahmedobaid23 <xtremepokem@gmail.com>
2025-01-22 04:24:08 +0500

    Add types and modify db

--- my-app/apps/http/src/router/v1/index.ts
+++ my-app/apps/http/src/router/v1/index.ts
@@ -2,13 +2,33 @@ import { Router } from "express";
 import { adminRouter } from "./admin";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
+import { SignupSchema } from "../../types";
+
+import client from "@repo/db/client";
 
 export const router = Router();
 
-router.post("/signup", (req, res) => {
-  res.json({
-    message: "Signup",
-  });
+router.post("/signup", async (req, res) => {
+  const parsedData = SignupSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({ message: "Validation Failed" });
+    return;
+  }
+
+  try {
+    const user = await client.user.create({
+      data: {
+        username: parsedData.data.username,
+        password: parsedData.data.password,
+        role: parsedData.data.type === "admin" ? "Admin" : "User",
+      },
+    });
+    res.status(200).json({ userId: user.id });
+    return;
+  } catch (error) {
+    res.status(400).json({ message: "User already exists" });
+    return;
+  }
 });
 
 router.post("/signin", (req, res) => {
